336838e0026b2dcf1ee6869cf0c7f99e370164a7
Natalia Favareto <natalia.favareto@hotmail.com>
2019-05-27 19:48:57 -0300

    Initial commit

---
+++ Assets/Camera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Camera : NetworkBehaviour 
+{
+    [SerializeField] private GameObject mainCamera;
+    [SerializeField] private GameObject clientCamera;
+
+	void Start () 
+    {
+        if (this.isServer)
+        {
+            mainCamera.SetActive(true);
+            clientCamera.SetActive(false);
+        }
+        else if (this.isClient)
+        {
+            mainCamera.SetActive(false);
+            clientCamera.SetActive(true);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/NetworkController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkController : NetworkBehaviour 
+{
+    [SerializeField] private NetworkManager networkManager;
+
+    public void Server()
+    {
+        NetworkClient host = networkManager.StartHost();
+
+        if (host.isConnected)
+            Debug.Log("Host conectado.");
+        else
+            Debug.Log("Erro ao conectar Host.");
+    }
+
+    public void Client()
+    {
+        NetworkClient client = networkManager.StartClient();
+        if (client.isConnected)
+            Debug.Log("Cliente conectado.");
+        else
+            Debug.Log("Erro ao conectar Cliente.");
+    }
+}
